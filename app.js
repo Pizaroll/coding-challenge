7ba871b4306be13679caa6f2a58b2fb4ff358d9f
Brittany Pizarro <88292020+Pizaroll@users.noreply.github.com>
2025-07-30 13:44:48 -0500

    First commit
    Testing and implementing changes.

---
+++ app.js
@@ -0,0 +1,16 @@
+// Slide data array
+
+const slides = [
+    {
+        title: "The Roberts Family Story",
+        description: "Brandon and Jennifer experienced many setbacks and obstacles before finally finding stability and a foundation to rebuild from at City Rescue Mission.",
+        imgURL: "https://img.youtube.com/vi/QSWXiRiTHWA/maxresdefault.jpg",
+        videoUrl: "https://youtu.be/QSWXiRiTHWA"
+    },
+    {
+        title: "Jamie's Story",
+        description: "Jamie and her 3 children faced eviction after fleeing domestic violence, and were searching for compassion and support during their season of homelessness. Watch how they found hope and restoration after walking through the doors of City Rescue Mission.",
+        imgURL: "https://img.youtube.com/vi/zFCh8IvKZSo/maxresdefault.jpg",
+        videoUrl: "https://youtu.be/zFCh8IvKZSo"
+    }
+];
