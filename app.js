20c9c01b9a0681522ab074e91bcb0342d38b91fe
Brittany Pizarro <brittanyrpizarro@gmail.com>
2025-07-31 19:19:15 -0500

    Testing Splide js video extension

--- app.js
+++ app.js
@@ -6,44 +6,46 @@ const slides = [
     description:
       "Brandon and Jennifer experienced many setbacks and obstacles before finally finding stability and a foundation to rebuild from at City Rescue Mission.",
     imgURL: "https://img.youtube.com/vi/QSWXiRiTHWA/maxresdefault.jpg",
-    videoUrl: "https://youtu.be/QSWXiRiTHWA",
+    videoId: "https://youtu.be/QSWXiRiTHWA?si=tU9X70ByzoS9PAzj",
   },
   {
     title: "Jamie's Story",
     description:
       "Jamie and her 3 children faced eviction after fleeing domestic violence, and were searching for compassion and support during their season of homelessness. Watch how they found hope and restoration after walking through the doors of City Rescue Mission.",
     imgURL: "https://img.youtube.com/vi/zFCh8IvKZSo/maxresdefault.jpg",
-    videoUrl: "https://youtu.be/zFCh8IvKZSo",
+    videoId: "https://youtu.be/zFCh8IvKZSo" 
   },
 ];
 // Setup of slide content
 document.addEventListener("DOMContentLoaded", () => {
   const list = document.querySelector("#story-carousel .splide__list");
 
-  // Inject slides into carousel
-  slides.forEach((slide) => {
-    const li = document.createElement("li");
-    li.className = "splide__slide";
-    li.innerHTML = `
-          <a href="${slide.videoUrl}" target="_blank" rel="noopener noreferrer">
-            <img src="${slide.imgURL}" alt="${slide.title}" />
-          </a>
-        `;
-    list.appendChild(li);
-  });
+// Inject slides into carousel
+slides.forEach((slide) => {
+  const li = document.createElement("li");
+  li.className = "splide__slide";
+  li.innerHTML = `
+      <a href="${slide.videoId}">
+        <img src="${slide.imgURL}" alt="${slide.title}" />
+      </a>
+    `;
+  list.appendChild(li);
+});
 
   // Initialize Splide without built-in arrows
   const splide = new Splide("#story-carousel", {
+    heightRatio: 0.5625,
+    cover      : true,
     type: "loop",
     perPage: 1,
     arrows: false,
     pagination: true,
-    autoplay: true,
+    autoplay: false,
     pauseOnHover: true,
     gap: "1rem",
   });
-
-  splide.mount();
+  splide.mount( window.splide.Extensions );
+  
 
   // Setup of custom button listeners
   const btnPrev = document.getElementById("btnPrev");
@@ -61,11 +63,13 @@ document.addEventListener("DOMContentLoaded", () => {
   // Elements for title and description
   const titleEl = document.getElementById("title");
   const descEl = document.getElementById("description");
+  const descMobileEl = document.getElementById("description-mobile");
 
   // Update title/description based on current slide index
   function updateInfo(index) {
     titleEl.textContent = slides[index].title;
     descEl.textContent = slides[index].description;
+    descMobileEl.textContent = slides[index].description;
   }
 
   // Initialize with first slide info
